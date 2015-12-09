d64b2570cda74641595bf70f27b80c7f6c8c081e
William <WilliamLiang918@gmail.com>
2015-12-09 15:58:51 -0500

    Refactor AttrAccessor method.

--- active_record_lite_skeleton/lib/00_attr_accessor_object.rb
+++ active_record_lite_skeleton/lib/00_attr_accessor_object.rb
@@ -1,22 +1,13 @@
 class AttrAccessorObject
+
   def self.my_attr_accessor(*names)
-
-    my_attr_reader(*names)
-    my_attr_writer(*names)
-  end
-    # ...
-  def self.my_attr_reader(*attrs)
-    attrs.each do |attr|
-      define_method(attr) do
-        instance_variable_get("@#{attr}")
+    names.each do |name|
+      define_method(name) do
+        instance_variable_get("@#{name}")
       end
-    end
-  end
 
-  def self.my_attr_writer(*attrs)
-    attrs.each do |attr|
-      define_method("#{attr}=") do |value|
-        instance_variable_set("@#{attr}", value)
+      define_method("#{name}=") do |value|
+        instance_variable_set("@#{name}", value)
       end
     end
   end
